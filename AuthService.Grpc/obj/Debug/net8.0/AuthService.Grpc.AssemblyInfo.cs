4900ac1e14b20b38b3b8a95a473aa28276052270
Muhammed Yasar S <muhammedyasars810@gmail.com>
2025-10-27 15:18:56 +0530

    database update

--- AuthService.Grpc/obj/Debug/net8.0/AuthService.Grpc.AssemblyInfo.cs
+++ AuthService.Grpc/obj/Debug/net8.0/AuthService.Grpc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Grpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40b9a392166d71a766b5757302c83c2e3044c058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8975fd67eabd4dd2fc3fad93015f8019c0eea049")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Grpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Grpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
